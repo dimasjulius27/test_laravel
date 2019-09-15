263e649051b14f39c244606733b50f8a8284891b
julius gigih dimastyo <dimasjulius27@gmail.com>
2019-09-15 08:54:13 +0700

    kedua

--- app/Http/Controllers/pegawaiController.php
+++ app/Http/Controllers/pegawaiController.php
@@ -10,7 +10,7 @@ class pegawaiController extends Controller
     {
     	// return "Halo ini adalah method index, dalam controller Pegawai";
     	$nama = 'Dimastyo';
-    	$arr_matkul = ['Analgor', 'kalkulus', 'RPL'];
+    	$arr_matkul = ['Analgor', 'kalkulus', 'RPL', 'Basis Data'];
     	return view('biodata',['nama'=>$nama, 'arr_matkul'=>$arr_matkul]);
     }
 }
